fe0bc1dda2903ee0598b595a5e546c6191acb68a
Preben Ver Eecke <preben_ve@hotmail.com>
2016-03-18 18:49:33 +0100

    Made Host class with basic params

---
+++ app/src/main/java/com/example/preben/portscanbuddy/Host.java
@@ -0,0 +1,21 @@
+package com.example.preben.portscanbuddy;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Preben on 18/03/2016.
+ */
+public class Host {
+
+    private ArrayList<Integer> ports;
+    private String ip;
+    private String hostName;
+
+    public Host(String ip)
+    {
+        this.ip = ip;
+    }
+
+
+
+}
