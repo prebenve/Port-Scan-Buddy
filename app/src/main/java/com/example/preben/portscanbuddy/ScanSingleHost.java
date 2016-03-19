ea21398d4a54d73aaa878b1d04810c5f781e0a1b
Preben Ver Eecke <preben_ve@hotmail.com>
2016-03-19 21:10:44 +0100

    updated multi port scan system, still buggy

--- app/src/main/java/com/example/preben/portscanbuddy/ScanSingleHost.java
+++ app/src/main/java/com/example/preben/portscanbuddy/ScanSingleHost.java
@@ -5,7 +5,10 @@ import android.util.Log;
 import android.widget.TextView;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by Preben on 19/03/2016.
@@ -14,13 +17,17 @@ public class ScanSingleHost {
 
     private Host host;
 
-
     public ScanSingleHost(Host host)
     {
         this.host = host;
 
-        new ScanPort().execute(445); //scant poort445
+        ArrayList<Integer> ports = new ArrayList<>(Arrays.asList(22, 80, 8000, 3600, 8082, 445, 3306));
+        ArrayList<AsyncTask> runningScans = new ArrayList<>();
 
+        for (int port : ports)
+        {
+            runningScans.add(new ScanPort().execute(port));
+        }
     }
 
     //// TODO: 19/03/2016 foreach parameter.length portscan?
@@ -34,7 +41,6 @@ public class ScanSingleHost {
 
             try {
                 Socket socket = new Socket(host.getIp(), portNumber);
-                
                 return socket.isConnected();
 
             } catch (IOException e) {
